ccaff3b3f392dd5ff84b62a77b248fe46645bbdd
Cortland Walker <cortlandjwalker@icloud.com>
2025-11-19 03:40:11 -0600

    Added App.kt and updated manifest

---
+++ demo/src/main/java/com/klipy/conversationdemo/App.kt
@@ -0,0 +1,18 @@
+package com.klipy.conversationdemo
+
+import android.app.Application
+import com.klipy.klipy_ui.KlipyUi
+import com.klipy.sdk.KlipySdk
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val repo = KlipySdk.create(
+            context = this,
+            secretKey = "gBAJiSCmnYiDLeGoUSBddk8FwuWFaLDMJ24vUBlalQS4IkCoBpznFZPZpBj1QZfh",
+            enableLogging = true
+        )
+
+        KlipyUi.configure(repo)
+    }
+}
